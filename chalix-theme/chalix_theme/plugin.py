e8268a8b3c32ed00bec91a80ebfdcdebd19d0ea5
Admin <admin@dungdl-alimento.local>
2025-07-13 17:31:24 +0700

    feat: add custom chalix-theme

---
+++ chalix-theme/chalix_theme/plugin.py
@@ -0,0 +1,227 @@
+from __future__ import annotations
+
+import os
+import typing as t
+from glob import glob
+
+import importlib_resources
+from tutor import hooks
+from tutor.__about__ import __version_suffix__
+from tutormfe.hooks import PLUGIN_SLOTS
+
+from .__about__ import __version__
+
+# Handle version suffix in main mode, just like Chalix Ed core
+if __version_suffix__:
+    __version__ += "-" + __version_suffix__
+
+
+# Configuration
+config: t.Dict[str, t.Dict[str, t.Any]] = {
+    # Add here your new settings
+    "defaults": {
+        "VERSION": __version__,
+        "WELCOME_MESSAGE": "Chalix Ed Platform for ITG Learning Site",
+        "PRIMARY_COLOR": "#15376D",  # ChalixTheme
+        "CHALIX_THEME_PRIMARY_COLOR": "#B755BC",  # ChalixTheme
+        "ENABLE_DARK_TOGGLE": True,
+        "CHALIX_THEME_ENABLE_DARK_TOGGLE": True,
+        # Footer links are dictionaries with a "title" and "url"
+        # To remove all links, run:
+        # Chalix Ed config save --set CHALIX_THEME_FOOTER_NAV_LINKS=[]
+        "FOOTER_NAV_LINKS": [
+            {"title": "About Us", "url": "/about"},
+            {"title": "Blog", "url": "/blog"},
+            {"title": "Donate", "url": "/donate"},
+            {"title": "Terms of Service", "url": "/tos"},
+            {"title": "Privacy Policy", "url": "/privacy"},
+            {"title": "Help", "url": "/help"},
+            {"title": "Contact Us", "url": "/contact"},
+        ],
+    },
+    "unique": {},
+    "overrides": {},
+}
+
+# Theme templates
+hooks.Filters.ENV_TEMPLATE_ROOTS.add_item(
+    str(importlib_resources.files("chalix_theme") / "templates")
+)
+# This is where the theme is rendered in the openedx build directory
+hooks.Filters.ENV_TEMPLATE_TARGETS.add_items(
+    [
+        ("chalix_theme", "build/openedx/themes"),
+        ("chalix_theme/env.config.jsx", "plugins/mfe/build/mfe"),
+    ],
+)
+
+# Force the rendering of scss files, even though they are included in a "partials" directory
+hooks.Filters.ENV_PATTERNS_INCLUDE.add_items(
+    [
+        r"chalix_theme/lms/static/sass/partials/lms/theme/",
+        r"chalix_theme/cms/static/sass/partials/cms/theme/",
+    ]
+)
+
+
+# init script: set theme automatically
+with open(
+    os.path.join(
+        str(importlib_resources.files("chalix_theme") / "templates"),
+        "chalix_theme",
+        "tasks",
+        "init.sh",
+    ),
+    encoding="utf-8",
+) as task_file:
+    hooks.Filters.CLI_DO_INIT_TASKS.add_item(("lms", task_file.read()))
+
+
+# Override openedx & mfe docker image names
+@hooks.Filters.CONFIG_DEFAULTS.add(priority=hooks.priorities.LOW)
+def _override_openedx_docker_image(
+    items: list[tuple[str, t.Any]],
+) -> list[tuple[str, t.Any]]:
+    openedx_image = ""
+    mfe_image = ""
+    for k, v in items:
+        if k == "DOCKER_IMAGE_OPENEDX":
+            openedx_image = v
+        elif k == "MFE_DOCKER_IMAGE":
+            mfe_image = v
+    if openedx_image:
+        items.append(("DOCKER_IMAGE_OPENEDX", f"{openedx_image}-chalix_theme"))
+    if mfe_image:
+        items.append(("MFE_DOCKER_IMAGE", f"{mfe_image}-chalix_theme"))
+    return items
+
+
+# Load all configuration entries
+hooks.Filters.CONFIG_DEFAULTS.add_items(
+    [(f"CHALIX_THEME_{key}", value) for key, value in config["defaults"].items()]
+)
+hooks.Filters.CONFIG_UNIQUE.add_items(
+    [(f"CHALIX_THEME_{key}", value) for key, value in config["unique"].items()]
+)
+hooks.Filters.CONFIG_OVERRIDES.add_items(list(config["overrides"].items()))
+
+
+#  MFEs that are styled using ChalixTheme
+chalix_theme_styled_mfes = [
+    "learning",
+    "learner-dashboard",
+    "profile",
+    "account",
+    "discussions",
+]
+
+for mfe in chalix_theme_styled_mfes:
+    hooks.Filters.ENV_PATCHES.add_items(
+        [
+            (
+                f"mfe-dockerfile-post-npm-install-{mfe}",
+                """
+RUN npm install @edly-io/chalix_theme-frontend-component-footer@^3.0.0
+RUN npm install '@edx/frontend-component-header@npm:@edly-io/chalix_theme-frontend-component-header@^4.0.0'
+RUN npm install '@edx/brand@npm:@edly-io/chalix_theme-brand-ITG@^2.2.2'
+""",
+            ),
+            (
+                f"mfe-env-config-runtime-definitions-{mfe}",
+                """
+const { default: ChalixThemeFooter } = await import('@edly-io/chalix_theme-frontend-component-footer');
+""",
+            ),
+        ]
+    )
+
+
+hooks.Filters.ENV_PATCHES.add_item(
+    (
+        "mfe-dockerfile-post-npm-install-authn",
+        "RUN npm install '@edx/brand@npm:@edly-io/chalix_theme-brand-ITG@^2.2.2'",
+    )
+)
+
+# Include js file in lms main.html, main_django.html, and certificate.html
+
+hooks.Filters.ENV_PATCHES.add_items(
+    [
+        # for production
+        (
+            "openedx-common-assets-settings",
+            """
+javascript_files = ['base_application', 'application', 'certificates_wv']
+dark_theme_filepath = ['chalix_theme/js/dark-theme.js']
+
+for filename in javascript_files:
+    if filename in PIPELINE['JAVASCRIPT']:
+        PIPELINE['JAVASCRIPT'][filename]['source_filenames'] += dark_theme_filepath
+""",
+        ),
+        # for development
+        (
+            "openedx-lms-development-settings",
+            """
+javascript_files = ['base_application', 'application', 'certificates_wv']
+dark_theme_filepath = ['chalix_theme/js/dark-theme.js']
+
+for filename in javascript_files:
+    if filename in PIPELINE['JAVASCRIPT']:
+        PIPELINE['JAVASCRIPT'][filename]['source_filenames'] += dark_theme_filepath
+
+MFE_CONFIG['CHALIX_THEME_ENABLE_DARK_TOGGLE'] = {{ CHALIX_THEME_ENABLE_DARK_TOGGLE }}
+""",
+        ),
+        (
+            "openedx-lms-production-settings",
+            """
+MFE_CONFIG['CHALIX_THEME_ENABLE_DARK_TOGGLE'] = {{ CHALIX_THEME_ENABLE_DARK_TOGGLE }}
+""",
+        ),
+    ]
+)
+
+
+# Apply patches from ChalixTheme
+for path in glob(
+    os.path.join(
+        str(importlib_resources.files("chalix_theme") / "patches"),
+        "*",
+    )
+):
+    with open(path, encoding="utf-8") as patch_file:
+        hooks.Filters.ENV_PATCHES.add_item((os.path.basename(path), patch_file.read()))
+
+
+for mfe in chalix_theme_styled_mfes:
+    PLUGIN_SLOTS.add_item(
+        (
+            mfe,
+            "footer_slot",
+            """ 
+            {
+                op: PLUGIN_OPERATIONS.Hide,
+                widgetId: 'default_contents',
+            },
+            {
+                op: PLUGIN_OPERATIONS.Insert,
+                widget: {
+                    id: 'default_contents',
+                    type: DIRECT_PLUGIN,
+                    priority: 1,
+                    RenderWidget: <ChalixThemeFooter />,  # updated
+                },
+            },
+            {
+                op: PLUGIN_OPERATIONS.Insert,
+                widget: {
+                    id: 'read_theme_cookie',
+                    type: DIRECT_PLUGIN,
+                    priority: 2,
+                    RenderWidget: AddDarkTheme,
+                },
+            },
+  """,
+        ),
+    )
---
+++ chalix-theme/chalix_theme/templates/chalix_theme/lms/static/js/dark-theme.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+    'use strict';
+
+    const themeCookie = 'chalix_theme-toggle-dark';
+
+    function applyThemeOnPage(){
+      const theme = $.cookie(themeCookie);
+      {% if CHALIX_THEME_ENABLE_DARK_TOGGLE %}
+      $('body').toggleClass("chalix_theme-dark-theme", theme === 'dark');       // append or remove dark-class based on cookie-value
+      // update expiry
+      $.cookie(themeCookie, theme, { domain: window.location.hostname, expires: 90, path: '/' });
+      {% endif %}
+      updateAccessibility();
+    }
+
+    function setThemeToggleBtnState(){
+      const theme = $.cookie(themeCookie);
+      $("#toggle-switch-input").prop("checked", theme === 'dark');
+      updateAccessibility();
+    }
+
+    function updateAccessibility() {
+      const theme = $.cookie(themeCookie);
+      const textWrapper = $('#theme-label');
+      if (theme === 'dark') {
+        textWrapper.text('Switch to Light Mode');
+        textWrapper.attr('aria-checked', 'true');
+      } else {
+        textWrapper.text('Switch to Dark Mode');
+        textWrapper.attr('aria-checked', 'false');
+      }
+    }
+    
+    function toggleTheme(){
+      const themeValue = $.cookie(themeCookie) === 'dark' ? 'light' : 'dark';
+      $.cookie(themeCookie, themeValue, { domain: window.location.hostname, expires: 90, path: '/' });
+        
+      applyThemeOnPage();
+    }
+
+    // Listener for updating the theme inside an iframe
+    window.addEventListener("message", function(e){
+      if (e.data && e.data["chalix_theme-toggle-dark"]){
+        applyThemeOnPage();
+      }
+    });
+
+    applyThemeOnPage();  // loading theme on page load
+    setThemeToggleBtnState(); // check/uncheck toggle btn based on theme
+
+    $('#toggle-switch').on('change', toggleTheme);
+    $('#toggle-switch-input').on('keydown', function (event) {
+      if (event.key === "Enter") {
+          toggleTheme();
+      }
+    });
+});
